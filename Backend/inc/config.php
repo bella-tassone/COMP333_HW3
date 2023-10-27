0b1e89fa5a60f1735e452020b8eb21183ee79468
bella.tassone <itassone@wesleyan.edu>
2023-10-27 14:38:17 -0400

    setting up config file for local development #1

--- Backend/inc/config.php
+++ Backend/inc/config.php
@@ -1,5 +1,5 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USERNAME", "demo");
-define("DB_PASSWORD", "demo");
-define("DB_DATABASE_NAME", "rest_api_demo");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE_NAME", "music_db");
