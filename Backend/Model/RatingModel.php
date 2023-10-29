9883988a3409aad70bfeb1b0a82dba761a1143ee
bella.tassone <itassone@wesleyan.edu>
2023-10-29 15:34:48 -0400

    setting up rating model #3

--- Backend/Model/RatingModel.php
+++ Backend/Model/RatingModel.php
@@ -8,19 +8,28 @@ require_once PROJECT_ROOT_PATH . "/Model/Database.php";
 
 class RatingModel extends Database
 {
-    // Creates a new user in the database
-    public function createRating($params) 
+    public function getRating($params)
     {
-        //synonymous to addRating.php
+        return $this->read("SELECT * FROM ratings WHERE username = ? AND song = ? AND artist = ?", ["sss", $params[0], $params[1], $params[2]]);
     }
 
-    public function deleteRating($params)
+    public function getRatingFromID($id) 
     {
-        //synonymous to deleteRating.php
+        return $this->read("SELECT * FROM ratings WHERE id = ?", ["i", $id]);
+    }
+
+    public function createRating($params) 
+    {
+        return $this->cud("INSERT INTO ratings (username, song, artist, rating) VALUES (?, ?, ?, ?)", ["sssi", $params[0], $params[1], $params[2], $params[3]]);
+    }
+
+    public function deleteRating($id)
+    {
+        return $this->cud("DELETE FROM ratings WHERE id = ?", ["i", $id]);
     }
 
     public function updateRating($params)
     {
-        //synonymous to updateRating.php
+        return $this->cud("UPDATE ratings SET song = ?, artist = ?, rating =? WHERE id = ?", ["ssii", $params[0], $params[1], $params[2], $params[3]]);
     }
 }
