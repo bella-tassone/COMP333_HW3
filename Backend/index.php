159ca75ad8425db35147c77210eb256dc9249cf5
Nate Levinson <natelevinson10@gmail.com>
2023-10-31 16:22:56 -0400

    fix CORS headers for backend (not making issue branch for simplicity)

--- Backend/index.php
+++ Backend/index.php
@@ -6,6 +6,15 @@ require __DIR__ . "/inc/bootstrap.php";
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 //CORS headers
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+    header('Access-Control-Allow-Origin:*');
+    header('Access-Control-Allow-Headers:*');
+    header('Access-Control-Allow-Methods:POST,GET,DELETE,PUT,OPTIONS');
+    header('Access-Control-Allow-Credentials:true');
+    exit;
+}
+
 header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Headers:*');
 header('Access-Control-Allow-Methods:POST,GET,DELETE,PUT,OPTIONS');
